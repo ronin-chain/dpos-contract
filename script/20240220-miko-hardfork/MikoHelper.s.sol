c0ef868ce75233398c81579c2e79cd5bd822993a
nxqbao <quocbao300@gmail.com>
2024-02-23 13:42:58 +0700

    script: nit

--- script/20240220-miko-hardfork/MikoHelper.s.sol
+++ script/20240220-miko-hardfork/MikoHelper.s.sol
@@ -27,19 +27,26 @@ abstract contract MikoHelper is RoninMigration {
 
   string public constant MIGRATE_DATA_PATH = "script/data/cid_mainnet.json";
 
+  /**
+   * Addresses that requires private keys
+   */
   address public constant DEPLOYER = 0xFE490b68E64B190B415Bb92F8D2F7566243E6ea0; // Mainnet Shadow deployer address;
   address public constant BAO_EOA = 0x4d58Ea7231c394d5804e8B06B1365915f906E27F;
   address public constant ADMIN_TMP_BRIDGE_TRACKING = 0x25F7D5901ed7d397EC0758bb59717d6D623286A1; // [Mainnet][Bridge] Bridge Tracking Temp Admin
   address public constant STAKING_MIGRATOR = 0x555a4D1201DecF7d5C87EcF67B1f0b6430bED2Ed; // [Mainnet][DPoS] Staking Migrator
-  address public constant TRUSTED_ORG_RECOVERY_LOGIC = 0x59646258Ec25CC329f5ce93223e0A50ccfA3e885;
 
-  TConsensus public constant STABLE_NODE_CONSENSUS = TConsensus.wrap(0x6E46924371d0e910769aaBE0d867590deAC20684);
-  address public constant STABLE_NODE_GOVERNOR = 0x3C583c0c97646a73843aE57b93f33e1995C8DC80;
+  /**
+   * Configs
+   */
+  address public constant TRUSTED_ORG_RECOVERY_LOGIC = 0x59646258Ec25CC329f5ce93223e0A50ccfA3e885;
 
   uint256 public constant PROPOSAL_DURATION = 14 days; // FIXME: `20 minutes` on mainnet shadow
   BridgeReward public constant DEPRECATED_BRIDGE_REWARD = BridgeReward(0x1C952D6717eBFd2E92E5f43Ef7C1c3f7677F007D);
 
   uint256 public constant PROFILE_PUBKEY_CHANGE_COOLDOWN = 1 days;
+
+  TConsensus public constant STABLE_NODE_CONSENSUS = TConsensus.wrap(0x6E46924371d0e910769aaBE0d867590deAC20684);
+  address public constant STABLE_NODE_GOVERNOR = 0x3C583c0c97646a73843aE57b93f33e1995C8DC80;
 
   function _parseMigrateData(
     string memory path
--- script/20240220-miko-hardfork/README.md
+++ script/20240220-miko-hardfork/README.md
@@ -10,11 +10,11 @@ The proposal will do the following tasks:
 - B1. Change admin of BridgeTracking to `doctor` (See [C2-4])
 - B2. Deploy all DPoS contracts and upgrade all DPoS contracts
 - B3. Reinitialize all DPoS contracts
+- C1. The `MIGRATOR_ROLE` in the Staking will migrate the list of `wasAdmin`.
 - B4. Replace StableNode's governor address
 - B5. Change governance admin of all contracts to (GANew)
 
 After the proposal is executed, the following scripts will be run:
-- C1. The `MIGRATOR_ROLE` in the Staking will migrate the list of `wasAdmin`.
 - C2. The `doctor` will withdraw the locked fund.
 - C3. The `doctor` will upgrade the Bridge Tracking contract to remove the recovery method.
 - C4. The `doctor` will transfer admin to BridgeManager.
