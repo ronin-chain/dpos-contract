1fefb99c8b32b700e2f925bb8f9330d6f02697d3
nxqbao <quocbao300@gmail.com>
2024-02-28 17:13:58 +0700

    script: fix final scripts on shadow mainnet

--- script/20240220-miko-hardfork/20240220_Full_shadow_Miko_Hardfork.s.sol
+++ script/20240220-miko-hardfork/20240220_Full_shadow_Miko_Hardfork.s.sol
@@ -38,16 +38,25 @@ contract Proposal__Full_20240220_MikoHardfork_ProposeProposal is
   {
     Proposal__Base_20240220_MikoHardfork.run();
 
+    vm.startPrank(DEPLOYER);
+    payable(0x529502C69356E9f48C8D5427B030020941F9ef42).transfer(10 ether);
+    payable(0x85C5dBfadcBc36AeE39DD32365183c5E38A67E37).transfer(10 ether);
+    payable(0x947AB99ad90302b5ec1840c9b5CF4205554C72af).transfer(10 ether);
+    payable(0x6a0397bDF0275Ad0201174afc05D3CFa27A5e1f1).transfer(10 ether);
+    payable(0x96262418638f93119429b8824fc22DFe7f428063).transfer(10 ether);
+    payable(0x32dA26032Ef488Ffe7d5A4Af23FD3bbBbCacA4C7).transfer(10 ether);
+    vm.stopPrank();
+
+    // CONFIG.setBroadcastDisableStatus(true);
+    // Proposal__20240220_MikoHardfork_Before._run_unchained(); // BAO_EOA
+
+    // CONFIG.setBroadcastDisableStatus(false);
+    // Proposal__20240220_MikoHardfork_ProposeProposal._run_unchained(); // Governor
+
+    // CONFIG.setBroadcastDisableStatus(false);
+    // Proposal__20240220_MikoHardfork_After._run_unchained(); // DOCTOR
+
     CONFIG.setBroadcastDisableStatus(false);
-    Proposal__20240220_MikoHardfork_Before._run_unchained(); // BAO_EOA
-
-    CONFIG.setBroadcastDisableStatus(true);
-    Proposal__20240220_MikoHardfork_ProposeProposal._run_unchained(); // Governor
-
-    CONFIG.setBroadcastDisableStatus(true);
-    Proposal__20240220_MikoHardfork_After._run_unchained(); // DOCTOR
-
-    CONFIG.setBroadcastDisableStatus(true);
     Proposal__20240220_MikoHardfork_Stable._run_unchained(); // MIGRATOR
   }
 
