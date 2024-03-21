d8e5abe2336eaaff43b3d243c8c1f082f2b218e4
nxqbao <quocbao300@gmail.com>
2024-03-21 11:35:46 +0700

    feat(ProfileMainnet): add fix migration method

--- contracts/ronin/profile/Profile_Mainnet.sol
+++ contracts/ronin/profile/Profile_Mainnet.sol
@@ -11,12 +11,14 @@ contract Profile_Mainnet is Profile {
   }
 
   function migrateOldConsensusList(address[] calldata lId, TConsensus[] calldata lCss) external onlyAdmin {
+    if (block.chainid != 2020) return;
     require(lId.length == lCss.length, "Invalid length");
 
     CandidateProfile storage _profile;
     for (uint i; i < lCss.length; ++i) {
       _profile = _id2Profile[lId[i]];
       _profile.oldConsensus = lCss[i];
+      _consensus2Id[lCss[i]] = lId[i];
     }
   }
 
