cdc1d70ec9489b0c4288ec37feea91d2168dab17
nxqbao <quocbao300@gmail.com>
2024-03-21 11:45:34 +0700

    fix(ProfileMainnet): simplify migration method

--- contracts/ronin/profile/Profile_Mainnet.sol
+++ contracts/ronin/profile/Profile_Mainnet.sol
@@ -10,15 +10,22 @@ contract Profile_Mainnet is Profile {
     _;
   }
 
-  function migrateOldConsensusList(address[] calldata lId, TConsensus[] calldata lCss) external onlyAdmin {
+  function migrateOmissionREP4() external onlyAdmin {
     if (block.chainid != 2020) return;
-    require(lId.length == lCss.length, "Invalid length");
+    address[] memory omittedCssList = new address[](3);
+
+    omittedCssList[0] = 0x454f6C34F0cfAdF1733044Fdf8B06516BD1E9529;
+    omittedCssList[1] = 0xD7fEf73d95ccEdb26483fd3C6C48393e50708159;
+    omittedCssList[2] = 0xbD4bf317Da1928CC2f9f4DA9006401f3944A0Ab5;
 
     CandidateProfile storage _profile;
-    for (uint i; i < lCss.length; ++i) {
-      _profile = _id2Profile[lId[i]];
-      _profile.oldConsensus = lCss[i];
-      _consensus2Id[lCss[i]] = lId[i];
+    for (uint i; i < omittedCssList.length; ++i) {
+      address id = omittedCssList[i];
+      TConsensus css = TConsensus.wrap(id);
+
+      _profile = _id2Profile[id];
+      _profile.oldConsensus = css;
+      _consensus2Id[css] = id;
     }
   }
 }
