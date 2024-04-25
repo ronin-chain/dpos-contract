b338cfb14ec425b6adadd29994f53cd4e79db12a
tu-do.ron <tudo.dev@gmail.com>
2024-04-25 12:18:45 +0700

    Update LibSLA.sol
    Co-authored-by: HuyHuynh <63286199+huyhuynh3103@users.noreply.github.com>
    Signed-off-by: tu-do.ron <tudo.dev@gmail.com>

--- contracts/libraries/LibSLA.sol
+++ contracts/libraries/LibSLA.sol
@@ -24,7 +24,7 @@ library LibSLA {
     return uint256(keccak256(abi.encode(req.period, req.prevBeacon, keyHash, oracle)));
   }
 
-  function hash(uint256[2] memory pk) internal pure returns (bytes32) {
-    return keccak256(abi.encode(pk));
+  function keyHash(uint256[2] memory publicKeys) internal pure returns (bytes32) {
+    return keccak256(abi.encode(publicKeys));
   }
 }
