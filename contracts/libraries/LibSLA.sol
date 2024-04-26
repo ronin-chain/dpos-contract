9a386495708cf43db913bfa6f9808d3edda72285
TuDo1403 <tudo.dev@gmail.com>
2024-04-26 15:26:33 +0700

    doc(LibSLA): update doc

--- contracts/libraries/LibSLA.sol
+++ contracts/libraries/LibSLA.sol
@@ -16,14 +16,23 @@ struct RandomRequest {
 using LibSLA for RandomRequest global;
 
 library LibSLA {
+  /**
+   * @dev Hashes the random request.
+   */
   function hash(RandomRequest memory req) internal pure returns (bytes32) {
     return keccak256(abi.encode(req.period, req.prevBeacon));
   }
 
+  /**
+   * @dev Calculates the proof seed
+   */
   function calcProofSeed(RandomRequest memory req, bytes32 keyHash, address oracle) internal pure returns (uint256) {
     return uint256(keccak256(abi.encode(req.period, req.prevBeacon, keyHash, oracle)));
   }
 
+  /**
+   * @dev Calculates the key hash from public keys.
+   */
   function calcKeyHash(uint256[2] memory publicKeys) internal pure returns (bytes32) {
     return keccak256(abi.encode(publicKeys));
   }
