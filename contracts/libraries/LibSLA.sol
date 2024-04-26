189d53f058aea3d085751ca980276d0fdf115d24
TuDo1403 <tudo.dev@gmail.com>
2024-04-26 15:10:29 +0700

    fix(LibSLA): rename keyHash -> calcKeyHash

--- contracts/libraries/LibSLA.sol
+++ contracts/libraries/LibSLA.sol
@@ -24,7 +24,7 @@ library LibSLA {
     return uint256(keccak256(abi.encode(req.period, req.prevBeacon, keyHash, oracle)));
   }
 
-  function keyHash(uint256[2] memory publicKeys) internal pure returns (bytes32) {
+  function calcKeyHash(uint256[2] memory publicKeys) internal pure returns (bytes32) {
     return keccak256(abi.encode(publicKeys));
   }
 }
