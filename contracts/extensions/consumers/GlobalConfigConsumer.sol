c9f5fb9b0248f5095e3de7c116f027114d0be4e2
TuDo1403 <tudo.dev@gmail.com>
2024-05-27 14:57:29 +0700

    feat(GlobalConfigConsumer): bump gas to 6200

--- contracts/extensions/consumers/GlobalConfigConsumer.sol
+++ contracts/extensions/consumers/GlobalConfigConsumer.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.9;
 
 abstract contract GlobalConfigConsumer {
-  /// @dev The addition amount of gas sending along in external calls. Total gas stipend is added with default 7300 gas.
-  uint256 public constant DEFAULT_ADDITION_GAS = 7300;
+  /// @dev The addition amount of gas sending along in external calls. Total gas stipend is added with default 2300 gas.
+  uint256 public constant DEFAULT_ADDITION_GAS = 6200;
   /// @dev The length of a period in second.
   uint256 public constant PERIOD_DURATION = 1 days;
 }
