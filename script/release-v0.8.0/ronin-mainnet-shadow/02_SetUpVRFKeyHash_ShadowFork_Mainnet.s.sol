28f0c55bd17539f4e782793c1367bc19b8977c1c
TuDo1403 <tudo.dev@gmail.com>
2024-06-04 21:04:48 +0700

    script: add script to setup vrf key hash on shadow fork mainnet

---
+++ script/release-v0.8.0/ronin-mainnet-shadow/02_SetUpVRFKeyHash_ShadowFork_Mainnet.s.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { RoninTrustedOrganization } from "@ronin/contracts/multi-chains/RoninTrustedOrganization.sol";
+import { IRoninTrustedOrganization } from "@ronin/contracts/interfaces/IRoninTrustedOrganization.sol";
+import { Profile } from "@ronin/contracts/ronin/profile/Profile.sol";
+import { TConsensus } from "@ronin/contracts/udvts/Types.sol";
+
+import { console } from "forge-std/console.sol";
+import { StdStyle } from "forge-std/StdStyle.sol";
+import { RoninMigration } from "script/RoninMigration.s.sol";
+import { Contract } from "script/utils/Contract.sol";
+import { Network } from "script/utils/Network.sol";
+
+import { LibString } from "@solady/utils/LibString.sol";
+import { LibVRFProof } from "script/shared/libraries/LibVRFProof.sol";
+import { LibPrecompile } from "script/shared/libraries/LibPrecompile.sol";
+import { LibWrapUpEpoch } from "script/shared/libraries/LibWrapUpEpoch.sol";
+
+contract Migration_02_SetupVRFKeyHash_ShadowFork_Mainnet is RoninMigration {
+  using LibVRFProof for *;
+  using StdStyle for *;
+
+  Profile private profile;
+  RoninTrustedOrganization private trustedOrg;
+  LibVRFProof.VRFKey[] private keys;
+
+  function run() public onlyOn(Network.ShadowForkMainnet.key()) {
+    profile = Profile(loadContract(Contract.Profile.key()));
+    trustedOrg = RoninTrustedOrganization(loadContract(Contract.RoninTrustedOrganization.key()));
+
+    IRoninTrustedOrganization.TrustedOrganization[] memory allTrustedOrgs = trustedOrg.getAllTrustedOrganizations();
+
+    uint256 govCount = allTrustedOrgs.length;
+
+    LibVRFProof.VRFKey[] memory _keys = LibVRFProof.genVRFKeys(govCount);
+    for (uint256 i; i < _keys.length; ++i) {
+      keys.push(_keys[i]);
+    }
+
+    for (uint256 i; i < govCount; ++i) {
+      address cid = profile.getConsensus2Id(allTrustedOrgs[i].consensusAddr);
+      address admin = profile.getId2Admin(cid);
+
+      console.log("\n");
+      console.log(string.concat("[gov-", vm.toString(i), "]"));
+      console.log("Admin =".yellow(), admin);
+      console.log("Consensus =".yellow(), TConsensus.unwrap(allTrustedOrgs[i].consensusAddr));
+      console.log("CID =".yellow(), cid);
+      console.log("KeyHash =".yellow(), vm.toString(keys[i].keyHash));
+      console.log("SecretKey =".yellow(), vm.toString(keys[i].secretKey));
+      console.log("Oracle =".yellow(), keys[i].oracle);
+      console.log("PrivateKey =".yellow(), LibString.toHexString(keys[i].privateKey));
+
+      vm.broadcast(admin);
+      profile.changeVRFKeyHash(cid, keys[i].keyHash);
+    }
+  }
+
+  function _postCheck() internal virtual override {
+    LibPrecompile.deployPrecompile();
+    vme.setUserDefinedConfig("vrf-keys", abi.encode(keys));
+    LibWrapUpEpoch.wrapUpPeriod();
+  }
+}
