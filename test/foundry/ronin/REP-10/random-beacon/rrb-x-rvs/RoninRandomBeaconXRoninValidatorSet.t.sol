17b2f5b55443f25c7a1a8b17d995b67f5ecd9667
tu-do.ron <tudo.dev@gmail.com>
2024-06-07 11:35:00 +0700

    Update test/foundry/ronin/REP-10/random-beacon/rrb-x-rvs/RoninRandomBeaconXRoninValidatorSet.t.sol
    Co-authored-by: Bao <quocbao300@gmail.com>
    Signed-off-by: tu-do.ron <tudo.dev@gmail.com>

--- test/foundry/ronin/REP-10/random-beacon/rrb-x-rvs/RoninRandomBeaconXRoninValidatorSet.t.sol
+++ test/foundry/ronin/REP-10/random-beacon/rrb-x-rvs/RoninRandomBeaconXRoninValidatorSet.t.sol
@@ -164,6 +164,7 @@ contract RoninRandomBeaconXRoninValidatorSetTest is REP10_BaseTest {
       ) {
         (,, nonRotatingValidators, rotatingValidators,) =
           abi.decode(logs[i].data, (bool, uint256, address[], address[], address));
+          break;
       }
     }
 
