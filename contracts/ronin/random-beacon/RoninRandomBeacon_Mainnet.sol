02de5ae8332fe7a220c8ec374e7748686deb6758
TuDo1403 <tudo.dev@gmail.com>
2024-06-12 13:30:09 +0700

    doc(RoninRandomBeacon_Mainnet): add comment

--- contracts/ronin/random-beacon/RoninRandomBeacon_Mainnet.sol
+++ contracts/ronin/random-beacon/RoninRandomBeacon_Mainnet.sol
@@ -5,7 +5,13 @@ pragma solidity ^0.8.9;
 import "./RoninRandomBeacon.sol";
 
 contract RoninRandomBeacon_Mainnet is RoninRandomBeacon {
+  /**
+   * @dev Left empty on purpose of matching the contract version with the one in the testnet.
+   */
   function initializeV2() external reinitializer(2) { }
 
+  /**
+   * @dev Left empty on purpose of matching the contract version with the one in the testnet.
+   */
   function initializeV3() external reinitializer(3) { }
 }
