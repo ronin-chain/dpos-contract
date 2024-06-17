53f54f36671ed81fed5a969cbb5089aa610330e8
TuDo1403 <tudo.dev@gmail.com>
2024-06-17 11:28:16 +0700

    script: fix wrap up epoch

--- test/foundry/ronin/REP-10/random-beacon/rrb-x-rvs/RoninRandomBeaconXRoninValidatorSet.t.sol
+++ test/foundry/ronin/REP-10/random-beacon/rrb-x-rvs/RoninRandomBeaconXRoninValidatorSet.t.sol
@@ -4,6 +4,149 @@ pragma solidity ^0.8.13;
 import "../../REP-10_Base.t.sol";
 
 contract RoninRandomBeaconXRoninValidatorSetTest is REP10_BaseTest {
+  function setUp() public override {
+    super.setUp();
+
+    IRandomBeacon.ValidatorType[] memory vTypes = new IRandomBeacon.ValidatorType[](4);
+    vTypes[0] = IRandomBeacon.ValidatorType.All;
+    vTypes[1] = IRandomBeacon.ValidatorType.Governing;
+    vTypes[2] = IRandomBeacon.ValidatorType.Standard;
+    vTypes[3] = IRandomBeacon.ValidatorType.Rotating;
+
+    uint256[] memory vThresholds = new uint256[](4);
+    vThresholds[0] = 5;
+    vThresholds[1] = 2;
+    vThresholds[2] = 0;
+    vThresholds[3] = 3;
+
+    vm.prank(governanceAdmin);
+    TransparentUpgradeableProxyV2(payable(address(roninRandomBeacon))).functionDelegateCall(
+      abi.encodeCall(RoninRandomBeacon.bulkSetValidatorThresholds, (vTypes, vThresholds))
+    );
+
+    vm.warp(_bound(vm.getBlockTimestamp(), vm.unixTime() / 1_000, type(uint40).max));
+    vme.rollUpTo(1000);
+
+    LibWrapUpEpoch.wrapUpPeriod();
+  }
+
+  function testConcrete_WrapUpPeriod_MustShareRewardCorrectly() public {
+    vm.deal(address(roninValidatorSet), 10_000_000 ether);
+    vm.deal(address(stakingVesting), 100_000_000 ether);
+
+    LibWrapUpEpoch.fastForwardToNextDay();
+    LibWrapUpEpoch.wrapUpEpoch();
+
+    address[] memory allCids = roninValidatorSet.getValidatorCandidateIds();
+    address[] memory admins = profile.getManyId2Admin(allCids);
+    TConsensus[] memory allCssLst = roninValidatorSet.getValidatorCandidates();
+
+    uint256[] memory balancesBefore = new uint256[](allCids.length);
+    for (uint256 i; i < allCids.length; i++) {
+      balancesBefore[i] = admins[i].balance;
+    }
+
+    uint256 currPeriod = roninValidatorSet.currentPeriod();
+    uint256 submittedBlockCount;
+
+    // for (uint256 i; i < 4; ++i) {
+    for (uint256 i; i < 144; ++i) {
+      TConsensus[] memory currValidators = roninValidatorSet.getValidators();
+      uint256 currBlockNumber = vm.getBlockNumber();
+
+      for (uint256 k; k < 200; ++k) {
+        uint j = k % currValidators.length;
+        vm.coinbase(TConsensus.unwrap(currValidators[j]));
+        // Random from 0.5 -> 1 RON using vm.unixTime();
+        // uint256 reward = (vm.unixTime() % 0.5 ether) + 0.5 ether;
+        uint256 reward = 0;
+
+        vm.roll(currBlockNumber + k);
+        vm.deal(TConsensus.unwrap(currValidators[j]), reward);
+        vm.prank(TConsensus.unwrap(currValidators[j]));
+        roninValidatorSet.submitBlockReward{ value: reward }();
+        submittedBlockCount++;
+
+        // console.log("Timestampppppppppppppp", vm.getBlockTimestamp());
+
+        vm.prank(TConsensus.unwrap(currValidators[j]));
+        // fastFinalityTracking.recordFinality(currValidators);
+        fastFinalityTracking.recordFinality(allCssLst);
+      }
+      // for (uint256 j; j < currValidators.length; ++j) {
+
+      // }
+
+      if (i == 143) {
+        console.log("Break for wrap period:", i);
+        break;
+      }
+
+      // LibWrapUpEpoch.fastForwardToNextEpoch();
+      LibWrapUpEpoch.wrapUpEpoch();
+
+      console.log("Current epoch:", roninValidatorSet.epochOf(block.number));
+      console.log("Current periodddddddd:", roninValidatorSet.currentPeriod());
+
+      if (currPeriod != roninValidatorSet.currentPeriod()) {
+        console.log("Break at epoch:", i);
+        revert();
+        break;
+      }
+    }
+
+    // uint256 currBlockNumber = vm.getBlockNumber();
+
+    // TConsensus[] memory currValidators = roninValidatorSet.getValidators();
+    // for (uint256 j; j < currValidators.length; ++j) {
+    //   vm.coinbase(TConsensus.unwrap(currValidators[j]));
+    //   // Random from 0.5 -> 1 RON using vm.unixTime();
+    //   uint256 reward = 0 ether;
+
+    //   vm.roll(currBlockNumber + j);
+    //   vm.deal(TConsensus.unwrap(currValidators[j]), reward);
+    //   vm.prank(TConsensus.unwrap(currValidators[j]));
+    //   roninValidatorSet.submitBlockReward{ value: reward }();
+    //   vm.prank(TConsensus.unwrap(currValidators[j]));
+    //   fastFinalityTracking.recordFinality(currValidators);
+    // }
+
+    // LibWrapUpEpoch.wrapUpEpoch();
+    // // LibWrapUpEpoch.fastForwardToNextDay();
+    // // LibWrapUpEpoch.fastForwardToNextEpoch();
+    uint256 nextDayTimestamp = vm.getBlockTimestamp() + 1 days;
+    vm.warp(nextDayTimestamp);
+    LibWrapUpEpoch.wrapUpEpoch();
+
+    uint256[] memory balancesAfter = new uint256[](allCids.length);
+
+    uint256 balanceChanged;
+    uint256 balanceUnchanged;
+    uint256 totalAllChanges;
+
+    for (uint256 i = 0; i < admins.length; i++) {
+      balancesAfter[i] = admins[i].balance;
+      if (balancesAfter[i] > balancesBefore[i]) {
+        totalAllChanges += balancesAfter[i] - balancesBefore[i];
+        balanceChanged++;
+      } else {
+        balanceUnchanged++;
+      }
+    }
+
+    uint256 maxValidatorNumber = roninValidatorSet.maxValidatorNumber();
+    console.log("Max validator count", maxValidatorNumber);
+    console.log("Total candidate count", allCids.length);
+    console.log("Balance changed count", balanceChanged);
+    console.log("Balance unchanged count", balanceUnchanged);
+    console.log("Submitted block count", submittedBlockCount);
+
+    console.log("Expected total all changes", submittedBlockCount * stakingVesting.blockProducerBlockBonus(0));
+    console.log("Total all changes", totalAllChanges);
+
+    assertTrue(balanceChanged > maxValidatorNumber, "Balance changed count must be greater than maxValidatorNumber");
+  }
+
   function testFuzz_getBlockProducers_And_getValidators_MustBeTheSame(uint16 wrapUpEpochCount) external {
     vm.deal(address(roninValidatorSet), 10_000_000 ether);
     vm.deal(address(stakingVesting), 100_000_000 ether);
