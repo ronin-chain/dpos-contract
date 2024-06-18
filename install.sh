6d819f7460f3c5d546b8d51ed29f2d23e50661de
nxqbao <quocbao300@gmail.com>
2024-06-18 15:03:46 +0700

    chore: fix install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-# Install foundryup
-curl -L https://foundry.paradigm.xyz | bash
-# Install foundry
-$HOME/.foundry/bin/foundryup -v nightly-de33b6af53005037b463318d2628b5cfcaf39916 # Stable version
-# Install rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+#!/bin/bash
+
+# Check if foundry is installed
+if ! command -v $HOME/.foundry/bin/forge &> /dev/null
+then
+    # Install foundryup
+    curl -L https://foundry.paradigm.xyz | bash
+    # Install foundry
+    $HOME/.foundry/bin/foundryup -v nightly-de33b6af53005037b463318d2628b5cfcaf39916 # Stable version
+fi
+
+# Check if rustup is installed
+if ! command -v rustup &> /dev/null
+then
+    # Install rustup
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
 # Update rustup
 $HOME/.cargo/bin/rustup update stable
 # Install soldeer
@@ -12,7 +24,17 @@ $HOME/.cargo/bin/cargo install soldeer
 $HOME/.cargo/bin/soldeer update
 # Run forge build
 $HOME/.foundry/bin/forge build
-# Install jq
-brew install jq
-# Install yq
-brew install yq
+
+# Check if rustup is installed
+if ! command -v jq &> /dev/null
+then
+    # Install jq
+    brew install jq
+fi
+
+# Check if rustup is installed
+if ! command -v yq &> /dev/null
+then
+    # Install yq
+    brew install yq
+fi
