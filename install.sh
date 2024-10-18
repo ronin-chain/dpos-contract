eb0ace845faa2ab1518bd6a52353d08349675065
TuDo1403 <tudo.dev@gmail.com>
2024-10-19 01:46:58 +0700

    chore: fix config

--- install.sh
+++ install.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
 
 # Check if foundry is installed
-if ! command -v $HOME/.foundry/bin/forge &> /dev/null
-then
+if ! command -v $HOME/.foundry/bin/forge &>/dev/null; then
     # Install foundryup
     curl -L https://foundry.paradigm.xyz | bash
     # Install foundry
-    $HOME/.foundry/bin/foundryup -v nightly-de33b6af53005037b463318d2628b5cfcaf39916 # Stable version
+    $HOME/.foundry/bin/foundryup
 fi
 
-# Check if rustup is installed
-if ! command -v rustup &> /dev/null
-then
-    # Install rustup
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-fi
-
-# Update rustup
-$HOME/.cargo/bin/rustup update stable
-# Install soldeer
-$HOME/.cargo/bin/cargo install soldeer
-# Update dependencies with soldeer
-$HOME/.cargo/bin/soldeer update
+$HOME/.foundry/bin/forge soldeer update
 # Run forge build
 $HOME/.foundry/bin/forge build
 
-# Check if rustup is installed
-if ! command -v jq &> /dev/null
-then
+# Check if homebrew is installed
+if ! command -v brew &>/dev/null; then
+    # Install homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Check if jq is installed
+if ! command -v jq &>/dev/null; then
     # Install jq
     brew install jq
 fi
 
-# Check if rustup is installed
-if ! command -v yq &> /dev/null
-then
+# Check if yq is installed
+if ! command -v yq &>/dev/null; then
     # Install yq
     brew install yq
 fi
+
+# Check if gvm is installed
+if ! command -v gvm &>/dev/null; then
+    # Install bison
+    brew install bison
+    # Install gvm
+    bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+fi
+
+# Source gvm script
+source ~/.gvm/scripts/gvm
+# Install go1.19
+gvm install go1.19 -B
+
+rm -rf temp
+mkdir temp
+cd temp
+ls
+
+git clone git@github.com:ronin-chain/ronin-random-beacon.git
+
+cd ronin-random-beacon
+
+gvm use go1.19 && go build -o ronin-random-beacon
+
+cp ronin-random-beacon ../../bin/ && cd ../../
+
+rm -rf temp
