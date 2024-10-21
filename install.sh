3f5f8b1aaf326b9e0a3a1ccf01c6cfb6594ec29a
TuDo1403 <tudo.dev@gmail.com>
2024-10-21 13:36:09 +0700

    ci: fix ci

--- install.sh
+++ install.sh
@@ -1,59 +1,50 @@
 #!/bin/bash
 
 # Check if foundry is installed
-if ! command -v $HOME/.foundry/bin/forge &>/dev/null; then
+if ! command -v forge &>/dev/null; then
     # Install foundryup
     curl -L https://foundry.paradigm.xyz | bash
     # Install foundry
     $HOME/.foundry/bin/foundryup
-fi
 
-$HOME/.foundry/bin/forge soldeer update
-# Run forge build
-$HOME/.foundry/bin/forge build
-
-# Check if homebrew is installed
-if ! command -v brew &>/dev/null; then
-    # Install homebrew
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    $HOME/.foundry/bin/forge soldeer update
+    # Run forge build
+    $HOME/.foundry/bin/forge build
 fi
 
 # Check if jq is installed
 if ! command -v jq &>/dev/null; then
     # Install jq
-    brew install jq
+    sudo apt-get install jq
 fi
 
 # Check if yq is installed
 if ! command -v yq &>/dev/null; then
-    # Install yq
-    brew install yq
+    curl -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/bin/yq &&
+        sudo chmod +x /usr/bin/yq
 fi
 
 # Check if gvm is installed
-if ! command -v gvm &>/dev/null; then
+if ! command -v go &>/dev/null; then
     # Install bison
-    brew install bison
+    sudo apt-get install bison
     # Install gvm
     bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+    # Source gvm script
+    source $HOME/.gvm/scripts/gvm
+    sudo apt-get install bsdmainutils
+
+    # Install go1.19
+    gvm install go1.19 -B
 fi
 
-# Source gvm script
-source ~/.gvm/scripts/gvm
-# Install go1.19
-gvm install go1.19 -B
+rm -rf temp && mkdir temp && cd temp
 
-rm -rf temp
-mkdir temp
-cd temp
-ls
+git clone https://github.com/ronin-chain/ronin-random-beacon.git && cd ronin-random-beacon
 
-git clone git@github.com:ronin-chain/ronin-random-beacon.git
+# Check if installed gvm
+if ! command -v gvm &>/dev/null; then
+    gvm use go1.19
+fi
 
-cd ronin-random-beacon
-
-gvm use go1.19 && go build -o ronin-random-beacon
-
-cp ronin-random-beacon ../../bin/ && cd ../../
-
-rm -rf temp
+go build -o ronin-random-beacon && cp ronin-random-beacon ../../bin/ && cd ../../ && rm -rf temp
