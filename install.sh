3cfb9321dd623494b6a169a8b6b7d1464d4f7a59
TuDo1403 <tudo.dev@gmail.com>
2024-10-24 14:17:57 +0700

    ci: add ci for slither-analyze, storage-layout-check, unit-test and rework githook

--- install.sh
+++ install.sh
@@ -15,32 +15,31 @@ fi
 # Check if jq is installed
 if ! command -v jq &>/dev/null; then
     # Install jq
-    sudo apt-get install jq
+    brew install jq
 fi
 
 # Check if yq is installed
 if ! command -v yq &>/dev/null; then
-    curl -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -o /usr/bin/yq &&
-        sudo chmod +x /usr/bin/yq
+    brew install -y wget
+    sudo wget https://github.com/mikefarah/yq/releases/download/v4.34.1/yq_linux_amd64 -O /usr/local/bin/yq
+    sudo chmod +x /usr/local/bin/yq
 fi
 
 # Check if gvm is installed
-if ! command -v go &>/dev/null; then
+if ! command -v gvm &>/dev/null; then
     # Install bison
-    sudo apt-get install bison
+    brew install bison
     # Install gvm
     bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
     # Source gvm script
     source $HOME/.gvm/scripts/gvm
-    sudo apt-get install bsdmainutils
+    brew install bsdmainutils
 
     # Install go1.19
     gvm install go1.19 -B
 fi
 
-rm -rf temp && mkdir temp && cd temp
-
-git clone https://github.com/ronin-chain/ronin-random-beacon.git && cd ronin-random-beacon
+rm -rf temp && mkdir temp && cd temp && git clone https://github.com/ronin-chain/ronin-random-beacon.git && cd ronin-random-beacon
 
 # Check if installed gvm
 if ! command -v gvm &>/dev/null; then
@@ -48,3 +47,5 @@ if ! command -v gvm &>/dev/null; then
 fi
 
 go build -o ronin-random-beacon && cp ronin-random-beacon ../../bin/ && cd ../../ && rm -rf temp
+
+echo "Installed successfully!"
