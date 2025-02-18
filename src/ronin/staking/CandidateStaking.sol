cfb1a5d662a106a0f7379d64ced8f287ce50dc01
Tu Do <tudo.dev@gmail.com>
2025-02-18 17:46:10 +0700

    fix(Staking): add more check when change admin addr

--- src/ronin/staking/CandidateStaking.sol
+++ src/ronin/staking/CandidateStaking.sol
@@ -282,6 +282,9 @@ abstract contract CandidateStaking is BaseStaking, ICandidateStaking, GlobalConf
     uint256 stakingAmount = _pool.stakingAmount;
     // ensure the new stakeholder is not a delegator with existing delegating amount
     if (_pool.delegatingAmount[newStakeholder] != 0) revert ErrAlreadyDelegator();
+    if (_pool.lastDelegatingTimestamp[newStakeholder] + _cooldownSecsToUndelegate > block.timestamp) {
+      revert ErrUnstakeTooEarly();
+    }
     _pool.lastDelegatingTimestamp[newStakeholder] = _pool.lastDelegatingTimestamp[requester];
 
     _changeDelegatingAmount(_pool, requester, 0, _pool.stakingTotal - stakingAmount);
